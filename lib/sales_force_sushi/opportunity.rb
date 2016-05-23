4f8c1f9e6a88fb8a655eb4ca367197c4ae0fcf96
voodoologic <headley.douglas@gmail.com>
2016-05-23 15:00:33 -0700

    removed some fields

--- lib/sales_force_sushi/opportunity.rb
+++ lib/sales_force_sushi/opportunity.rb
@@ -2,7 +2,7 @@ require 'json'
 module SalesForceSushi
   class Opportunity
     include SalesForceSushi::Utils
-    FIELDS =  %w[id account.name amount close_date description lead_source next_step name probability stage_name type zoho_id__c created_date]
+    FIELDS =  %w[id amount description lead_source name probability stage_name type zoho_id__c created_date]
     attr_accessor :id, :zoho_id__c, :account, :amount, :close_date, :contract, :description, :expected_revenue, :forcase_category_name,
       :last_modified_by, :lead_source, :next_step, :name, :owner, :record_type, :partner_account, :pricebook_2,
       :campain, :is_private, :probability, :total_opportunity_quality, :stage_name, :synced_quote, :type, :url,
